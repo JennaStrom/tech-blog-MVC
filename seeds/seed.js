f71d19e5b6e13542c75da9eaa65224382e7f984f
JennaStrom <123525191+JennaStrom@users.noreply.github.com>
2023-06-11 15:13:24 -0500

    getting started on the handlebar files

--- seeds/seed.js
+++ seeds/seed.js
@@ -5,3 +5,26 @@ const userData = require('./userData.json')
 const blogData = require('./blogData.json');
 const commentData = require('./commentData.json');
 
+const seedDB = async () => {
+    await sequelize.sync ({ force: true });
+
+    const users = await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    for (const blog of blogData) {
+        await Blog.create({
+            ...blog,
+            user_id: users[Math.floor(Math.random() * users.length)].id,
+        });
+    
+        const comments = await Comment.create(commentData, {
+            returning: true,
+        });
+    }
+
+    process.exit(0);
+};
+
+seedDB();
